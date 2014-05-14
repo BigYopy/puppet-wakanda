2925499657af594c86ff16ce9f1ba86e23f7b07b
Bigyopy <a.elhamdani90@gmail.com>
2014-05-14 17:32:46 +0100

    wakanda bin's name

--- manifests/linux/config.pp
+++ manifests/linux/config.pp
@@ -1,6 +1,16 @@
 class wakanda::linux::config {
   require wakanda::linux::install
   
+  ################################################################
+  #wakanda community's bin name is different than enterprise's one
+  #it may be better if they have the same name
+  
+  $wakanda_bin = $wakanda::enterprise ? {
+    true => 'wakandaenterprise',
+    default => wakanda,
+  }
+  ###################################################
+  
   #wakanda default file
   file { "/etc/default/wakanda":
     ensure => present,
@@ -10,7 +20,7 @@ class wakanda::linux::config {
   # create a link to wakanda bin
   file {"/usr/local/bin/wakanda":
     ensure => link,
-    target => "${wakanda::wakanda_dir}/bin/wakanda",
+    target => "${wakanda::wakanda_dir}/bin/${$wakanda_bin}",
   }
   
   # wakanda upstart job
